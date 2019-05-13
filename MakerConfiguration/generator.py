7853ed97c7000757d0236ce43d4618efaa81a336
Lufi Liu <ust.eg.lwm@gmail.com>
2019-05-13 10:46:26 +0800

    added progress update via database

--- MakerConfiguration/generator.py
+++ MakerConfiguration/generator.py
@@ -5,11 +5,16 @@ import time
 import logging
 
 #TODO: FInish the generate
-def generate(*args, **kwargs):
+def generate(mount_points, constraints, similarity_function, 
+             interrupt_flag, update_func, update_param):
     logging.basicConfig(level=logging.DEBUG)
     logging.debug("generate PASS THROUGH: Doing Nothing")
 
     for i in range(10):
-        time.sleep(3)
+        time.sleep(2)
         print("Time passed:", i)
+        update_func(update_type="progress", content="Time {}".format(i), 
+                    **update_param)
+        if interrupt_flag is True:
+            return {"status": False, "message": "Interrupted"}
     return {"status": True}
--- TaskManager/taskmanager.py
+++ TaskManager/taskmanager.py
@@ -36,14 +36,14 @@ class TaskManager(object):
             input_json TEXT NOT NULL
         );''')
         c.execute('''CREATE TABLE IF NOT EXISTS results(
-            result_id INTEGER PRIMARY KEY,          
-            result_json TEXT NOT NULL,
+            result_id INTEGER PRIMARY KEY,
+            progress TEXT,
+            result_json TEXT,
             input_id INTEGER NOT NULL,
                 FOREIGN KEY (input_id)  REFERENCES input_sets(input_id)
         );''')
         conn.commit()
         conn.close()
-
 
     def create_task(self, input_set):
         ret_validate = self.__validate(input_set)
@@ -51,11 +51,10 @@ class TaskManager(object):
             return ret_validate
         # TODO insert tasks and commit
 
+        query = "INSERT INTO input_sets (input_json) VALUES (?)"
         conn = sqlite3.connect(self.db_path)
         try:
-            c = conn.execute("INSERT INTO input_sets (input_json) VALUES (?)",
-                             (json.dumps(input_set),)
-            )
+            c = conn.execute(query, (json.dumps(input_set),))
             conn.commit()
             ret_dict = {
                 "status": True,
@@ -69,21 +68,27 @@ class TaskManager(object):
                 "message": repr(e)
             }
 
-    def start_task(self, task_id):
-        tid = str(task_id)
-        if self.running_tasks.get(tid) is not None:
-            if self.running_tasks.get(tid).process.is_alive():
+    def start_task(self, input_id):
+        # input_id and task_id is same for now, but maybe changed to 
+        # have multiple running tasks
+        task_id = input_id
+        if self.running_tasks.get(task_id) is not None:
+            if self.running_tasks.get(task_id).process.is_alive():
                 return {
                     "status": False,
-                    "message": "task_id {} is already running.".format(task_id)
+                    "message": "input_id {} is already running.".format(input_id)
                 }
-        logging.debug("start_task, task_id: {}".format(task_id))
-        task_data = self.get_task(task_id)
+        logging.debug("start_task, input_id: {}".format(input_id))
+
+        task_data, task_id = self.get_task_and_allocate_tid(input_id)
+        if task_data.get("status") is False:
+            return task_data
+
         interrupt_flag = Value(ctypes.c_bool)
         params_dict = {
             "interrupt_flag": interrupt_flag,
-            "save_func": TaskManager.save_result,
-            "save_param": {
+            "update_func": TaskManager.update_task,
+            "update_param": {
                 "db_path": self.db_path,
                 "task_id": task_id,
             }
@@ -92,14 +97,25 @@ class TaskManager(object):
         p = Process(target=self.__execute, kwargs=params_dict) 
         p.start()
         t = Task(p, interrupt_flag)
-        self.running_tasks[tid] = t
+        self.running_tasks[task_id] = t
         return {
             "status": True,
-            "task_id": tid
+            "task_id": task_id
         }
 
     def stop_task(self, task_id): 
-        pass
+        if self.running_tasks.get(task_id) is not None:
+            t = self.running_tasks.get(task_id)
+            if t.process.is_alive():
+                t.interrupt_flag = True
+                return {
+                    "status": True,
+                    "message": "interrupting task_id {}.".format(task_id)
+                }
+        return {
+            "status": True,
+            "message": "task_id {} not running".format(task_id)
+        }
 
     def list_available_tasks(self, ):
         pass
@@ -107,14 +123,57 @@ class TaskManager(object):
     def list_running_tasks(self, ):
         pass
 
-    def get_task(self, task_id):
-        # TODO: Finish this
-        logging.debug("get_task, task_id: {}".format(task_id))
-        return {"data": 0}
+    def get_task_and_allocate_tid(self, input_id):
+        logging.debug("get_task, input_id: {}".format(input_id))
+
+        try:
+            # Retrieve Data
+            conn = sqlite3.connect(self.db_path)
+            c = conn.cursor()
+            query = """SELECT input_json FROM input_sets WHERE input_id = ?"""
+            c.execute(query, (input_id,))
+            row = c.fetchone()
+            if row is None:
+                raise IndexError("input_id {} not found".format(input_id))
+            input_set = json.loads(row[0])
+        
+            query = """REPLACE INTO results
+                       (input_id, result_id, progress, result_json)
+                       VALUES (?, ?, "Initializing...", "")"""
+            c.execute(query, (input_id, input_id))
+            conn.commit()
+            ret_id = input_id
+        except Exception as e:
+            logging.debug("Error get_task: {}".format(e))
+            conn.close()
+            return {
+                "status": False,
+                "message": repr(e)
+            }, -1
+
+        conn.close()
+        return input_set, ret_id
 
     @staticmethod
-    def save_result(result, db_path, task_id):
-        # TODO: FInish this
-        logging.debug("save_result, task_id: {};  result: {}".format(
-            task_id, result))
+    def update_task(update_type, content, db_path, task_id):
+        """ update the task with update type and content"""
+        if update_type not in ["progress", "result_json"]:
+            return False
+        
+        logging.debug("update_task, task_id: {}; type: {}; data: {}".format(
+            task_id, update_type, content))
+        try:
+            # Update Data Data
+            conn = sqlite3.connect(db_path)
+            c = conn.cursor()
+            query = """UPDATE results
+                       SET {} = ?
+                       WHERE result_id = ?""".format(update_type)
+            c.execute(query, (content, task_id))
+            conn.commit()
+            conn.close()
+        except Exception as e:
+            logging.debug("Error get_task: {}".format(e))
+            conn.close()
+            return False
         return True
